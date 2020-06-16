5813735ce0a6fede60fcd1fbb056f7009eda4491
Evgheni Lisita <evghenii.lisita@gmail.com>
2020-06-16 18:49:01 +0200

    fisrt commit

---
+++ SwiftUI-Custon-Nav-An-Api222/SwiftUI-Custon-Nav-An-Api/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Custon-Nav-An-Api
+//
+//  Created by Evgheni Lisita on 16.06.20.
+//  Copyright © 2020 Evgheni Lisita. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
