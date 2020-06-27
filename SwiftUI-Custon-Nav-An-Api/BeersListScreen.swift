577cdc96f0740321d6c98e00cdd66117608c79fe
Evgheni Lisita <evghenii.lisita@gmail.com>
2020-06-27 15:51:30 +0200

    add api layer

---
+++ SwiftUI-Custon-Nav-An-Api/BeersListScreen.swift
@@ -0,0 +1,21 @@
+//
+//  BeersListScreen.swift
+//  SwiftUI-Custon-Nav-An-Api
+//
+//  Created by Evgheni Lisita on 27.06.20.
+//  Copyright © 2020 Evgheni Lisita. All rights reserved.
+//
+
+import SwiftUI
+
+struct BeersListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BeersListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        BeersListScreen()
+    }
+}
